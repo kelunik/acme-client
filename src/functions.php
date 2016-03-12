fa81d66686d495b3849d3c27a6180b06a03f73b0
Niklas Keller <me@kelunik.com>
2016-03-12 17:46:57 +0100

    Strip protocol from server key name

--- src/functions.php
+++ src/functions.php
@@ -49,6 +49,8 @@ function resolveServer($uri) {
 }
 
 function serverToKeyname($server) {
+    $server = substr($server, strpos("://", $server) + 3);
+
     $keyFile = str_replace("/", ".", $server);
     $keyFile = preg_replace("@[^a-z0-9._-]@", "", $keyFile);
     $keyFile = preg_replace("@\\.+@", ".", $keyFile);
