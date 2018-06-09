35070bb70afdb57eed7381f15f83829499d4a34b
Niklas Keller <me@kelunik.com>
2018-06-09 19:00:02 +0200

    Add logging support for debugging

--- src/AcmeFactory.php
+++ src/AcmeFactory.php
@@ -2,12 +2,27 @@
 
 namespace Kelunik\AcmeClient;
 
+use Amp\ByteStream\ResourceOutputStream;
+use Amp\Log\ConsoleFormatter;
+use Amp\Log\StreamHandler;
 use Kelunik\Acme\AcmeClient;
 use Kelunik\Acme\AcmeService;
 use Kelunik\Acme\Crypto\PrivateKey;
+use Monolog\Logger;
+use Monolog\Processor\PsrLogMessageProcessor;
 
 class AcmeFactory {
     public function build(string $directory, PrivateKey $keyPair): AcmeService {
-        return new AcmeService(new AcmeClient($directory, $keyPair));
+        $logger = null;
+        if (\getenv('ACME_LOG')) {
+            $logger = new Logger('acme');
+            $logger->pushProcessor(new PsrLogMessageProcessor);
+
+            $handler = new StreamHandler(new ResourceOutputStream(\STDERR));
+            $handler->setFormatter(new ConsoleFormatter(null, null, true, true));
+            $logger->pushHandler($handler);
+        }
+
+        return new AcmeService(new AcmeClient($directory, $keyPair, null, null, $logger));
     }
 }
